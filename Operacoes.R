dc02aa03037c9422add4e48e90c56164c3156d4b
Maxwell Alves <maxwell3531@gmail.com>
2021-04-02 15:02:46 -0300

    Utilizando Operações matematicas

---
+++ Operacoes.R
@@ -0,0 +1,60 @@
+# Operaçoes matematicas
+
+# Utilizando recursos basicos
+
+# Soma
+5 + 5
+
+# Subtraçao
+10 - 8
+
+# Divisao
+9 / 3
+
+# Multiplicaçao
+4 * 2
+
+# Potencia
+4 ** 2
+4 ^ 2
+
+# Igual?
+5 == 6
+5 == 5
+
+# Diferente?
+5 != 6
+5 != 5
+
+# Maior?
+4 > 2
+4 > 6
+
+# Menor?
+3 < 2
+3 < 4
+
+# Maior ou igual?
+4 >= 5
+4 >= 4
+
+#Menor ou igual?
+6 <= 8
+6 <= 5
+
+# E
+6 == 6
+6 == 6 & 7 == 8
+6 == 6 & 7 != 8
+5 > 3 & 5 < 8
+
+# OU
+6 == 6
+7 == 8
+6 == 6 | 7 == 8
+6 != 6 | 7 == 8
+5 > 3 | 5 < 8
+
+# Negaçao
+!6 == 6
+!4 > 2
